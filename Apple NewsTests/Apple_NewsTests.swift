62b8e47d858b6c8f76244e6166117cfce33c524a
Akhil <akhil.verma@leadsqaured.com>
2020-10-03 13:06:35 +0530

    Initial Commit

---
+++ Apple NewsTests/Apple_NewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Apple_NewsTests.swift
+//  Apple NewsTests
+//
+//  Created by Akhil on 03/10/20.
+//
+
+import XCTest
+@testable import Apple_News
+
+class Apple_NewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
